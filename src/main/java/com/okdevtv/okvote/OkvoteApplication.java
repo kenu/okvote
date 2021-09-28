0688f321e0b280ec0510c77bdb99b274f0d29688
kenu <kenu.heo@gmail.com>
2021-09-28 11:40:18 -0700

    init

---
+++ src/main/java/com/okdevtv/okvote/OkvoteApplication.java
@@ -0,0 +1,13 @@
+package com.okdevtv.okvote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkvoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkvoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/okdevtv/okvote/OkvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.okdevtv.okvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkvoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
