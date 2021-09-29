0d34210b4cfd34c5a70e970a827ccfae4809c643
kenu <kenu.heo@gmail.com>
2021-09-29 11:10:24 -0700

    layout #5

--- src/main/java/com/okdevtv/okvote/OkvoteApplication.java
+++ src/main/java/com/okdevtv/okvote/OkvoteApplication.java
@@ -2,12 +2,19 @@ package com.okdevtv.okvote;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
 
 @SpringBootApplication
 public class OkvoteApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(OkvoteApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(OkvoteApplication.class, args);
+  }
 
+  @Bean
+  public LayoutDialect layoutDialect() {
+    return new LayoutDialect();
+  }
 }
