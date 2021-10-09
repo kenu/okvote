869d3871729f5c39d5bdc78d5eed2dc5c0dbf9dc
seonja <seonja.kim0@gmail.com>
2021-10-09 17:20:30 +0900

    indentation

--- src/main/resources/react/src/pages/List.jsx
+++ src/main/resources/react/src/pages/List.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const List = () => {
-  const questions = ['Q 스프링 프레임워크는 자바를 몰라도 개발할 수 있다', 'Q 스프링 프레임워크는 자바를 몰라도 개발할 수 있다', 'Q 스프링 프레임워크는 자바를 몰라도 개발할 수 있다', 'Q 스프링 프레임워크는 자바를 몰라도 개발할 수 있다', 'Q 스프링 프레임워크는 자바를 몰라도 개발할 수 있다']
+  const questions = ['Q 스프링 프레임워크는 자바를 몰라도 개발할 수 있다', 
+                    'Q 스프링 프레임워크는 자바를 몰라도 개발할 수 있다', 
+                    'Q 스프링 프레임워크는 자바를 몰라도 개발할 수 있다', 
+                    'Q 스프링 프레임워크는 자바를 몰라도 개발할 수 있다', 
+                    'Q 스프링 프레임워크는 자바를 몰라도 개발할 수 있다']
 
   const questionList = questions.map((question, index) => (
     <div key={index} className="question">{question}</div>
--- src/main/resources/react/src/pages/Login.jsx
+++ src/main/resources/react/src/pages/Login.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Login = () => {
   return (
     <div>
-    <form method="POST" id="login-form">
-      <input type="text" name="id" id="id" />
-      <button class="login">로그인</button>
-    </form>
+      <form method="POST" id="login-form">
+        <input type="text" name="id" id="id" />
+        <button className="login">로그인</button>
+      </form>
     </div>
   );
 };
