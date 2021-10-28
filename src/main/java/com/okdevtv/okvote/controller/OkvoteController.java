45e9956d72893fac974f5bcbb1e8e8256c636b5e
kenu <kenu@okjsp.pe.kr>
2021-10-28 14:16:18 +0900

    로그인 처리 #30

--- src/main/java/com/okdevtv/okvote/controller/OkvoteController.java
+++ src/main/java/com/okdevtv/okvote/controller/OkvoteController.java
@@ -2,12 +2,15 @@ package com.okdevtv.okvote.controller;
 
 import com.okdevtv.okvote.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class OkvoteController {
@@ -27,23 +30,48 @@ public class OkvoteController {
     return "index";
   }
   @RequestMapping("/form")
-  public String form() {
+  public Object form(Model model, @CookieValue(name = "name", required = false) String name) {
+    if (name == null) {
+      return new RedirectView("/login");
+    }
+    model.addAttribute("name", name);
     return "form";
   }
+
   @PostMapping("/form")
   public String form(@RequestParam String question) {
     System.out.println(question);
     return "form";
   }
+
   @RequestMapping("/login")
   public String login() {
     return "login";
   }
-  @RequestMapping(method = RequestMethod.POST, path="/login")
-  public String loginProcess(@RequestParam String name) {
-    User user = userRepository.save(new User(name));
-    System.out.println(user.toString());
-    return "login";
+  @PostMapping("/login")
+  public RedirectView loginProcess(HttpServletRequest request, HttpServletResponse response, @RequestParam String name) {
+    String lowerName = name.toLowerCase();
+    Cookie cookie = new Cookie("name", lowerName);
+    cookie.setMaxAge(60 * 60 * 24);
+    cookie.setPath("/");
+    response.addCookie(cookie);
+    HttpSession session = request.getSession();
+    session.setAttribute("name", lowerName);
+    User userByName = userRepository.findUserByName(lowerName);
+    if (userByName == null) {
+      User user = userRepository.save(new User(lowerName));
+    }
+    return new RedirectView("/form");
+  }
+
+  @GetMapping("/logout")
+  public RedirectView logout(HttpServletRequest request, HttpServletResponse response) {
+    Cookie cookie = new Cookie("name", null);
+    cookie.setMaxAge(0);
+    cookie.setPath("/");
+    response.addCookie(cookie);
+    request.getSession().removeAttribute("name");
+    return new RedirectView("/");
   }
   @RequestMapping("/result")
   public String result() {
--- src/main/java/com/okdevtv/okvote/model/UserRepository.java
+++ src/main/java/com/okdevtv/okvote/model/UserRepository.java
@@ -1,6 +1,11 @@
 package com.okdevtv.okvote.model;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    @Query(value = "SELECT id, name FROM User u WHERE u.name = :name",
+            nativeQuery = true)
+    User findUserByName(@Param("name") String name);
 }
