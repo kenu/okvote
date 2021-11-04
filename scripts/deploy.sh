5f6576cdd949c26d015eeee76b0d2e8792f151be
kenu <kenu.heo@gmail.com>
2021-11-04 16:08:20 +0900

    배포 자동화 - restart 서버 #39

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,6 @@
 GITHUB=https://github.com/kenu/okvote
 REPOSITORY=~/git/okvote
 
-## build jar
-cd $REPOSITORY
-git pull origin main
-./mvnw clean package && echo "build.."
-
 ## kill process
 CURRENT_PID=$(pgrep -f okvote)
 if [ -z $CURRENT_PID ]
@@ -15,13 +10,19 @@ then
 else
   echo ">>>> PID: $CURRENT_PID kill."
   kill -15 $CURRENT_PID
-  sleep 15
+  sleep 30
 fi
+
+## build jar
+cd $REPOSITORY
+git pull origin main
+./mvnw clean package && echo "build.."
 
 ## start jar
 JAR_NAME=$(ls $REPOSITORY/target | grep jar | head -n 1)
 JAR_PATH=$REPOSITORY/target/$JAR_NAME
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+# nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar $JAR_PATH &
 
 sleep 10
 
