593090740e04dc8e642017f365642297856fa2e9
kenu <kenu@okjsp.pe.kr>
2021-11-04 09:18:46 +0900

    코드 배포 자동화 #36 - spring boot restart

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,22 @@ cd $REPOSITORY
 git pull origin main
 ./mvnw clean package && echo "build.."
 
+## kill process
+CURRENT_PID=$(pgrep -f okvote)
+if [ -z $CURRENT_PID ]
+then
+  echo ">>>> java process not found."
+else
+  echo ">>>> PID: $CURRENT_PID kill."
+  kill -15 $CURRENT_PID
+  sleep 10
+fi
+
+## start jar
 JAR_NAME=$(ls $REPOSITORY/target | grep jar | head -n 1)
 JAR_PATH=$REPOSITORY/target/$JAR_NAME
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+
+# curl -X POST -H 'Content-type: application/json' \
+# --data '{"text":"okvote Deployment Finished!\nhttps://v.okdevtv.com/"}' \
+# https://hooks.slack.com/services/x/x/xxx
