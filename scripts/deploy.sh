a5aa0d62fe8019f00d6c33026e6399847dd37bfb
kenu <kenu@okjsp.pe.kr>
2021-11-04 09:37:12 +0900

    코드 배포 자동화 #36 - spring boot restart

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,13 +15,15 @@ then
 else
   echo ">>>> PID: $CURRENT_PID kill."
   kill -15 $CURRENT_PID
-  sleep 10
+  sleep 15
 fi
 
 ## start jar
 JAR_NAME=$(ls $REPOSITORY/target | grep jar | head -n 1)
 JAR_PATH=$REPOSITORY/target/$JAR_NAME
 nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+
+sleep 10
 
 # curl -X POST -H 'Content-type: application/json' \
 # --data '{"text":"okvote Deployment Finished!\nhttps://v.okdevtv.com/"}' \
