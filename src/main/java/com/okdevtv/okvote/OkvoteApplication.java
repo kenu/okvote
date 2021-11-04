eeab45588ee43a8bff442982e6b9145fe0dbd0ba
kenu <kenu.heo@gmail.com>
2021-11-04 15:46:50 +0900

    footer link #45

--- src/main/java/com/okdevtv/okvote/OkvoteApplication.java
+++ src/main/java/com/okdevtv/okvote/OkvoteApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class OkvoteApplication {
