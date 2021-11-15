d3eaa5247e9fb6823e80d469f3b4a634ea869196
kenu <kenu.heo@gmail.com>
2021-11-15 16:32:01 +0900

    HikariPool-1 - Connection #52 config

--- src/main/java/com/okdevtv/okvote/OkvoteApplication.java
+++ src/main/java/com/okdevtv/okvote/OkvoteApplication.java
@@ -1,6 +1,5 @@
 package com.okdevtv.okvote;
 
-import io.sentry.Sentry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
